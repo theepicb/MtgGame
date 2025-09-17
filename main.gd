aaf92c370fd64c9078b0fdea9067de1eab36d3b1
theepicb <theepicb101@gmail.com>
2025-09-18 10:20:51 +1200

    Add save/load system and implement game state restoration
    Introduces functions for saving and loading game data, including player inventory, money, upgrades, and packs. Adds methods to restore game state from saved data or initialize defaults if no save is found. Refactors _ready() to use the new loading logic.

--- main.gd
+++ main.gd
@@ -9,26 +9,67 @@ func save_game():
 			"money_per_second": $Money_Clicker.money_per_second
 		},
 		"Player": {
-			"inventory": Player.inventory,
+			"inventory": Player.cardInventory,
 			"money": Player.money
-		}
+		},
+		"Upgrades": {
+			"avaliable": get_node("/root/Main/Upgrades").available_upgrades,
+			"purchased": get_node("/root/Main/Upgrades").purchased_upgrades
+		},
+		"Packs": get_node("/root/Pack_Screen").packs
+			
+		
 	}
 	var file = FileAccess.open(SAVE_PATH, FileAccess.WRITE)
 	file.store_string(JSON.stringify(data))
 	file.close()
-	
-	
 	pass
+
+func loadGame ():
+	if not FileAccess.file_exists(SAVE_PATH):
+		print("No Save Data Found")
+		return  null
+	
+	var file = FileAccess.open(SAVE_PATH, FileAccess.READ)
+	if file:
+		var file_content = file.get_as_text()
+		file.close()
+		
+		var data = JSON.parse_string(file)
+		return data
+	
+	else: return null;
+	
+	return null;
+	pass
+
+func implamentData (data: Dictionary):
+	Player.cardInventory = data["Player"].get("inventory", [])
+	Player.money = data["Player"].get("money", 0)
+	$Upgrades.available_upgrades = data["Upgrades"].get("avaliable", [])
+	$Upgrades.purchased_upgrades = data["Upgrades"].get("purchased", [])
+	$Money_Clicker.money_per_click = data["Money_Clicker"].get("money_per_click", 0.01)
+	$Money_Clicker.combo_timer = data["Money_Clicker"].get("combo_wait_time", 1)
+	$Money_Clicker.money_per_second = data["Money_Clicker"].get("momey_per_seceond", 0)
+	pass
+
+func defultLoad ():
+	Player.money = 0;
+	Player.inventory = [];
+	$Money_Clicker.money_per_click = 0.01;
+	$Money_Clicker.combo_wait_time = 1;
+	$Money_Clicker.money_multiplier = 1;
+	save_game();
 
 func _ready() -> void:
 	if not FileAccess.file_exists("user://game_data.json"):
-		Player.money = 0;
-		Player.inventory = [];
-		$Money_Clicker.money_per_click = 0.01;
-		$Money_Clicker.combo_wait_time = 1;
-		$Money_Clicker.money_multiplier = 1;
-		save_game();
+		defultLoad()
 		pass
-	
+	else:
+		var loading = loadGame()
+		if loading != null:
+			implamentData(loading)
+		else:
+			defultLoad()
 	var screen = 0;
 	pass
